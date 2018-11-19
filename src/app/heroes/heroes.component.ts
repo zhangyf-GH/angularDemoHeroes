c2e03515dcefaae86687070594d351f336ccb9ae
zhang yongfei <zhangyfzone@gmail.com>
2018-11-19 14:28:57 +0800

    添加删除英雄功能

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -25,6 +25,11 @@ export class HeroesComponent implements OnInit {
     }
     this.heroService.addHero({name} as Hero).subscribe(hero => {this.heroes.push(hero)});
   }
+
+  delete(hero:Hero): void{
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
